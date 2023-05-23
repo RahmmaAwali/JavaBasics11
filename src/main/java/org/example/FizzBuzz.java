c0d621a56380d748070e0f41745d2286857b4606
Rahmma Awali <rahmma.hassen273@gmail.com>
2023-05-23 18:49:31 -0400

    Fixed the order

--- src/main/java/org/example/FizzBuzz.java
+++ src/main/java/org/example/FizzBuzz.java
@@ -1,5 +1,27 @@
 package org.example;
-
+// by: Rahmma
+// Date:05/23/2023
 public class FizzBuzz {
+    public static void main(String[] args) {
+
+        int num = 100;
+
+        for (int i = 1; i <= num; i++) {
+
+            if (((i % 5) == 0) && ((i % 7) == 0))
+            {
+                System.out.println("fizzbuzz");
+            } else if ((i % 5) == 0)
+            {
+                System.out.println("fizz");
+            } else if ((i % 7) == 0)
+            {
+                System.out.println("buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+        
+    }
 }
 // commit and push
